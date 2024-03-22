d7b009c4aad56700684ac7f24b347040b6909b6a
Adam Knapp <aknapp124@gmail.com>
2024-03-21 18:53:36 -0700

    Uploads images to cloudinary

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,26 +1,41 @@
 'use client';
 
 import React from 'react';
-import picture from '../../../public/images/google-map.png';
+
+import axios from 'axios';
 
 export default function Page() {
+	async function uploadToCloudinary(blob) {
+		const instance = axios.create();
+
+		const data = new FormData();
+		data.append('file', blob);
+		data.append('cloud_name', 'dqh2jrg9q');
+		data.append('upload_preset', 'rjehvjob');
+
+		const res = await instance.post(
+			'https://api.cloudinary.com/v1_1/dqh2jrg9q/image/upload/',
+			data
+		);
+	}
+
 	async function videoStream() {
 		const video = document.querySelector('video');
 		const pictureElement = document.querySelector('.picture');
-		try {
-			const stream = await navigator.mediaDevices.getUserMedia({
-				video: {
-					width: 400,
-					height: 200,
-				},
-				audio: false,
-			});
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				width: 400,
+				height: 200,
+			},
+			audio: false,
+		});
 
-			video.srcObject = stream;
-			video.play();
-			console.log(stream);
+		video.srcObject = stream;
+		video.play();
+		console.log(stream);
 
-			document.addEventListener('keypress', async (e) => {
+		document.addEventListener('keypress', async (e) => {
+			try {
 				if (e.code !== 'KeyK') return;
 
 				const canvas = document.createElement('canvas');
@@ -32,46 +47,17 @@ export default function Page() {
 					.getContext('2d')
 					.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
 
-				let img = canvas
-					.toDataURL('image/png')
-					.replace('data:image/png;base64,', '');
-				pictureElement.src = img;
+				const blob = await new Promise((resolve) =>
+					canvas.toBlob(resolve, 'image/png')
+				);
 
-				function chunkString(str, size) {
-					const chunks = [];
-					for (let i = 0; i < str.length; i += size) {
-						chunks.push(str.slice(i, i + size));
-					}
-					return chunks;
-				}
-
-				try {
-					const chunkedString = chunkString(img, 1000);
-					for (let chunk of chunkedString) {
-						const response = await fetch('http://localhost:4000/autosave', {
-							method: 'POST',
-							headers: {
-								'Content-Type': 'text/plain',
-							},
-							body: chunk,
-						});
-
-						if (response.ok) {
-							console.log('Image saved successfully');
-						} else {
-							console.error('Failed to save image');
-						}
-					}
-				} catch (error) {
-					console.error('Error saving image: ', error);
-				}
-
-				let imageToSave = pictureElement.src;
-				imageToSave = 'data:image/png;base64,' + img;
-			});
-		} catch (error) {
-			console.log(error);
-		}
+				const imageURL = URL.createObjectURL(blob); // Convert blob to data URL
+				pictureElement.src = imageURL;
+				uploadToCloudinary(blob);
+			} catch (error) {
+				console.log(error);
+			}
+		});
 	}
 
 	return (
@@ -79,6 +65,7 @@ export default function Page() {
 			<section>
 				<div>Dashboard</div>
 				<video id='video'></video>
+				<picture></picture>
 				<img src={null} alt='picture' className='picture' />
 				<button onClick={videoStream}>Video</button>
 			</section>
