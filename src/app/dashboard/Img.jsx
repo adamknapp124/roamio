95be0c3db4c87ed1b6151f03608d1a68b93de053
Adam Knapp <aknapp124@gmail.com>
2024-03-30 21:03:04 -0700

    Added facial focus from cloudinary

--- src/app/dashboard/Img.jsx
+++ src/app/dashboard/Img.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Cloudinary } from '@cloudinary/url-gen';
 import { AdvancedImage } from '@cloudinary/react';
-import { thumbnail, scale } from '@cloudinary/url-gen/actions/resize';
-import { focusOn, FocusOn } from '@cloudinary/url-gen/qualifiers/gravity';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { focusOn } from '@cloudinary/url-gen/qualifiers/gravity';
+import { face } from '@cloudinary/url-gen/qualifiers/focusOn';
 
 const Img = ({ pid }) => {
 	const cld = new Cloudinary({ cloud: { cloudName: 'roamio' } });
 	const myImage = cld.image(pid);
-	myImage.resize(thumbnail().width(200).height(200));
+	myImage.resize(thumbnail().width(200).height(200).gravity(focusOn(face())));
 	return (
 		<>
 			<AdvancedImage cldImg={myImage} />
